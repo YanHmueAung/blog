529b56424ac9b8e1478cbb8696da8233edc5a154
yan <yanhmueaung@gmail.com>
2021-11-21 22:41:45 +0800

    lodash map

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
 import jsonPlaceholder from "../api/jsonPlaceholder"
 import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.map(getState().posts, 'userId')
+}
+
 export const fetchPosts = () => async (dispatch, getState) => {
     const response = await jsonPlaceholder.get('/posts');
     console.log(response.data)
@@ -12,6 +19,11 @@ export const fetchPosts = () => async (dispatch, getState) => {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
 //     dispatch({ type: 'FETCH_USER', payload: response.data });
 // }
+
+export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+}
 
 
 //In this way we can call the api only one time and can not get anymore;
