c2954de6ad294154de4cb1be3e673b61e502f837
yan <yanhmueaung@gmail.com>
2021-11-21 18:31:37 +0800

    user reducer and ownProps

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,7 @@ export const fetchPosts = () => async (dispatch, getState) => {
         payload: response.data
     })
 }
+export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+}
